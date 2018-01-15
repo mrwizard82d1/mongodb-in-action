986f23e4ef0538bf8874cdb2ec23eeaa32e5f7ee
Larry Jones <larryj@blinds.com>
2018-01-14 20:28:54 -0600

    Add function to save tweets for a tag

--- chap03/TweetArchiver/tweet_archiver.rb
+++ chap03/TweetArchiver/tweet_archiver.rb
@@ -1,6 +1,9 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'rubygems'
 require 'mongo'
 require 'twitter'
-require './config'
+require 'config'
 
 class TweetArchiver
   def initialize(tag)
@@ -16,6 +19,16 @@ class TweetArchiver
       config.consumer_secret = CONSUMER_SECRET
       config.access_token = TOKEN
       config.access_token_secret = TOKEN_SECRET
+    end
+  end
+
+  def save_tweets_for(term)
+    @client.search(term).each do |tweet|
+      @tweets_found += 1
+      tweet_doc = tweet.to_h
+      tweet_doc[:tags] = term
+      tweet_doc[:_id] = tweet_doc[:id]
+      @tweets.insert_one(tweet_doc)
     end
   end
 end
